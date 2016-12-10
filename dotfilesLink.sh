36893bae88a51c4c04f2f6c32126a560d2c3c882
ccbwaltz <airi@localhost.localdomain>
2016-12-10 23:58:31 +0900

    bash/shell update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,5 @@
 ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
 ln -sf ~/dotfiles/.emacs.d/Cask ~/.emacs.d/Cask
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
